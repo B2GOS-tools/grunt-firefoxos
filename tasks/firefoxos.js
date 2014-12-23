9e6846c98e8cfef5ebb378dbffb1d53716bc43b5
Daniel Coloma <dcoloma@tid.es>
2014-12-23 19:47:58 +0100

    Adapt implementation to the new promises paradigm use by node-firefoxos-cli

--- tasks/firefoxos.js
+++ tasks/firefoxos.js
@@ -3,20 +3,21 @@
 module.exports = function (grunt) {
   var ffos = require('node-firefoxos-cli');
   var path = require('path');
+  var Promise = require('promise');
 
   grunt.registerMultiTask('ffospush', 'Pushes an app to the device',
   function () {
     var done = this.async();
     var src = path.join(process.cwd(), this.data.zip);
+    console.log("Please accept the prompt in your device".blue)
 
-    ffos.installPackagedApp(this.data.appId, src, function (err) {
-      if (err) {
-        console.log('ERROR installing app'.red);
-      }
-      else {
-        console.log('App installed');
-      }
+    ffos.installPackagedApp(this.data.appId, src).then(function(){
+      console.log("Application Installed".green)
       done();
+    }, function(err){
+      console.log("Error Installing App".red);
+      console.log(err.red);
+      done(false);
     });
   });
 
@@ -35,27 +36,29 @@ module.exports = function (grunt) {
 
   grunt.registerMultiTask('ffosstop', 'Stops an application', function () {
     var done = this.async();
-    ffos.closeApp(this.data.appId, function (err) {
-      if (err) {
-        console.log('ERROR stopping app'.red);
-      }
-      else {
-        console.log('App stopped');
-      }
+
+    console.log("Please accept the prompt in your device".blue)
+    ffos.closeApp(this.data.appId).then(function(){
+      console.log("Application Stopped".green)
       done();
+    }, function(err){
+      console.log("Error Stopping App".red);
+      console.log(err.red);
+      done(false);
     });
   });
 
   grunt.registerMultiTask('ffoslaunch', 'Launches an application', function () {
     var done = this.async();
-    ffos.launchApp(this.data.appId, function (err) {
-      if (err) {
-        console.log('ERROR launching app'.red);
-      }
-      else {
-        console.log('App launched');
-      }
+
+    console.log("Please accept the prompt in your device".blue)
+    ffos.launchApp(this.data.appId).then(function(){
+      console.log("Application Launched".green)
       done();
+    }, function(err){
+      console.log("Error Launching App".red);
+      console.log(err.red);
+      done(false);
     });
   });
 };
