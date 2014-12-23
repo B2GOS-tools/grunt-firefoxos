eb865614f92df7fcca7a5bd3a41016b657136319
Daniel Coloma <dcoloma@tid.es>
2014-12-23 23:24:57 +0100

    Adapt library to changes in favour of Promises in node-firefoxos-cli

--- tasks/firefoxos.js
+++ tasks/firefoxos.js
@@ -3,34 +3,66 @@
 module.exports = function (grunt) {
   var ffos = require('node-firefoxos-cli');
   var path = require('path');
+  var Promise = require('promise');
 
   grunt.registerMultiTask('ffospush', 'Pushes an app to the device',
-  function () {
+  function() {
     var done = this.async();
     var src = path.join(process.cwd(), this.data.zip);
+    console.log("Please accept the prompt in your device".blue)
 
-    ffos.installPackagedApp(this.data.appId, src, function (err) {
-      if (err) {
-        console.log('ERROR installing app'.red);
-      }
-      else {
-        console.log('App installed');
-      }
+    ffos.installPackagedApp(this.data.appId, src).then(function() {
+      console.log("Application Installed".green)
       done();
+    }, function(err) {
+      console.log("Error Installing App".red);
+      console.log(err.red);
+      done(false);
     });
   });
 
-  grunt.registerTask('ffosreset', 'Resets B2G', function () {
+  grunt.registerTask('ffosreset', 'Resets B2G', function() {
     var done = this.async();
-    ffos.resetB2G(function () {
+    ffos.resetB2G().then(function() {
       console.log('B2G process reseted');
       done();
+    }, function(err) {
+      console.log("Error resetting B2G".red);
+      done(false);
     });
   });
 
-  grunt.registerTask('ffoslog', 'Outputs the log', function () {
+  grunt.registerTask('ffoslog', 'Outputs the log', function() {
     this.async();
     ffos.logcat();
+  });
+
+  grunt.registerMultiTask('ffosstop', 'Stops an application', function() {
+    var done = this.async();
+
+    console.log("Please accept the prompt in your device".blue)
+    ffos.closeApp(this.data.appId).then(function() {
+      console.log("Application Stopped".green)
+      done();
+    }, function(err) {
+      console.log("Error Stopping App".red);
+      console.log(err.red);
+      done(false);
+    });
+  });
+
+  grunt.registerMultiTask('ffoslaunch', 'Launches an application', function() {
+    var done = this.async();
+
+    console.log("Please accept the prompt in your device".blue)
+    ffos.launchApp(this.data.appId).then(function() {
+      console.log("Application Launched".green)
+      done();
+    }, function(err) {
+      console.log("Error Launching App".red);
+      console.log(err.red);
+      done(false);
+    });
   });
 };
 
